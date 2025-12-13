cfee9bb5018374b88a7b7c9525fde3beeafe295a
Malinda-Rathnayaka <rathnayakamalinda14@gmail.com>
2025-12-14 01:40:19 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MoongoDB');
+    
+}).catch((err) =>{
+    console.log('error');
+    
+})
 
 const app = express();
 
