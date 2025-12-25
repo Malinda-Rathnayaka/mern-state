05d7e7321eb15895eed92d8803d9df1cdfcdd2f2
Malinda-Rathnayaka <rathnayakamalinda14@gmail.com>
2025-12-26 03:28:57 +0530

    add reux persist (there is must setup storage that one is not YT project)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Fallbacks for environments where the imported `storage` doesn't expose getItem/setItem
+const noopStorage = {
+  getItem: (_key) => Promise.resolve(null),
+  setItem: (_key, _value) => Promise.resolve(),
+  removeItem: (_key) => Promise.resolve(),
+};
+
+const createLocalStorageWrapper = () => ({
+  getItem: (key) => Promise.resolve(window.localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(window.localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(window.localStorage.removeItem(key)),
+});
+
+const storageEngine =
+  storage && typeof storage.getItem === 'function'
+    ? storage
+    : typeof window !== 'undefined' && typeof window.localStorage !== 'undefined'
+    ? createLocalStorageWrapper()
+    : noopStorage;
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage: storageEngine,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer},
-  middleware:(getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck:false,//exp
-  }),
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
+export const persistor = persistStore(store);
