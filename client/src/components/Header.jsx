02d7a32707d16b65dc2ba978742d16d47167a188
Malinda-Rathnayaka <rathnayakamalinda14@gmail.com>
2026-01-08 23:41:00 +0530

    change header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,43 +2,49 @@ import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-
 export default function Header() {
-
-    const {currentUser} = useSelector(state => state.user)
+  const { currentUser } = useSelector(state => state.user);
 
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto'>
-            <Link to=''>
-            <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
-                <samp className='text-slate-500'>Sahand</samp>
-                <samp className='text-slate-700'>Estate</samp>
-            </h1>
-            </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                <FaSearch className='bg-slate-600'/>
-            </form>
-            <ul className='flex gap-4'>
-                <Link to='/'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
-                </Link>
-                <Link to='/about'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-                </Link>
-                <Link to='/profile'>
-                    {currentUser ? (
-                        <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
-                    ) :(
-                        <li className='text-slate-700 hover:underline'>Sign in</li>
-                    )}
-                    
-                </Link>
-            </ul>
+      <div className='flex justify-between items-center max-w-6xl mx-auto'>
+        <Link to=''>
+          <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+            <samp className='text-slate-500'>Sahand</samp>
+            <samp className='text-slate-700'>Estate</samp>
+          </h1>
+        </Link>
 
-        </div>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input
+            type="text"
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+          />
+          {/* 🔽 Only changed this icon */}
+          <FaSearch className='text-slate-500 text-lg cursor-pointer hover:text-slate-700' />
+        </form>
 
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+          </Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className='text-slate-700 hover:underline'>Sign in</li>
+            )}
+          </Link>
+        </ul>
+      </div>
     </header>
-  )
+  );
 }
