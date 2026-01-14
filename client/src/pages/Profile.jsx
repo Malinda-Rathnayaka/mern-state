f8f742de043b502136525db022bd76821592fc86
Malinda-Rathnayaka <rathnayakamalinda14@gmail.com>
2026-01-14 23:55:28 +0530

    create user delete function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { useRef } from "react"
 import { useState } from "react"
-import {updateUserFailure, updateUserSuccess, updateUserStart} from '../redux/user/userSlice'
+import {updateUserFailure, updateUserSuccess, updateUserStart, deleteUserStart} from '../redux/user/userSlice'
 import { useDispatch } from "react-redux"
 import { data } from "react-router-dom"
 
@@ -45,6 +45,18 @@ export default function Profile() {
     }
   }
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,
+      {
+        method:`DELETE`,
+      })
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -60,7 +72,7 @@ export default function Profile() {
       </form>
 
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ''}</p>
